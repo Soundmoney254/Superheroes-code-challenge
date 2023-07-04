931fe3bc067a58c00d983e7a07856c29b199ad19
Samuel <samnmbuguah@gmail.com>
2023-07-04 10:58:13 +0300

    Heroes Controller

--- app/controllers/hero_powers_controller.rb
+++ app/controllers/hero_powers_controller.rb
@@ -1,2 +1,18 @@
 class HeroPowersController < ApplicationController
+    def create
+        hero_power = HeroPower.new(hero_power_params)
+    
+        if hero_power.save
+          hero = Hero.find(hero_power.hero_id)
+          render json: hero, include: :powers
+        else
+          render json: { errors: hero_power.errors.full_messages }, status: :unprocessable_entity
+        end
+    end
+    
+    private
+    
+    def hero_power_params
+        params.permit(:strength, :power_id, :hero_id)
+    end
 end
--- app/controllers/powers_controller.rb
+++ app/controllers/powers_controller.rb
@@ -1,2 +1,40 @@
 class PowersController < ApplicationController
+    # GET /powers
+    def index
+      powers = Power.all
+      render json: powers
+    end
+  
+    # GET /powers/:id
+    def show
+      power = Power.find_by(id: params[:id])
+  
+      if power
+        render json: power
+      else
+        render json: { error: 'Power not found' }, status: :not_found
+      end
+    end
+  
+    # PATCH /powers/:id
+    def update
+      power = Power.find_by(id: params[:id])
+  
+      if power
+        if power.update(power_params)
+          render json: power
+        else
+          render json: { errors: power.errors.full_messages }, status: :unprocessable_entity
+        end
+      else
+        render json: { error: 'Power not found' }, status: :not_found
+      end
+    end
+  
+    private
+  
+    def power_params
+      params.permit(:description)
+    end
 end
+  
